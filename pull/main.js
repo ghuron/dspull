aa9e722c2237d3ebae8d28a400f18fe5861353be
Dmitry <40275556+ooppsss60@users.noreply.github.com>
2019-07-18 12:47:43 +0300

    Update main.js

--- pull/main.js
+++ pull/main.js
@@ -10,5 +10,27 @@ for (var i = 0; i<req.length;i++){
 			break;
 	}
 }
+var xhr = new XMLHttpRequest();
+
+// Синхронный запрос!!!
+xhr.open('GET', 'https://www.wikidata.org/w/api.php?action=wbgetentities&ids='+id+'&props=sitelinks&format=json&origin=*', false);
+
+xhr.send();
+if (xhr.status != 200) {
+  alert( xhr.status + ': ' + xhr.statusText ); 
+} else {
+  res = JSON.parse(xhr.response); 
+  console.log(res.entities[id].sitelinks[site]);
+}
+
+
+xhr.open('GET', 'https://ru.wikipedia.org/w/index.php?wbgetentities=%D0%9C%D0%BE%D0%B4%D1%83%D0%BB%D1%8C:TableTools&origin=*', false);
+
+xhr.send();
+if (xhr.status != 200) {
+  alert( xhr.status + ': ' + xhr.statusText ); 
+} else {
+  console.log(xhr.response);
+}
 
 
