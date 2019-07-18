59b6aeb5aee4ae91dacdbc451159b6cd11c0903d
Dmitry <40275556+ooppsss60@users.noreply.github.com>
2019-07-18 22:56:15 +0300

    Mergely diff added

--- pull/main.js
+++ pull/main.js
@@ -7,14 +7,14 @@ for (var i = 0; i<req.length;i++){
 	}
 }
 
+$(document).ready(function () {$('#mergely').mergely();});// initialize mergely
+
 $.get('https://www.wikidata.org/w/api.php?action=wbgetentities&ids='+id+'&props=sitelinks&format=json&origin=*', function(res) {
 
 	var mediawikiTitle = encodeURIComponent(res.entities[id].sitelinks["mediawikiwiki"].title).replace('%3A',':').replace('%20',' ');//Название модуля на MediaWiki
 	var mediawikiUrl = "https://www.mediawiki.org/w/api.php?format=json&action=query&titles=" + mediawikiTitle + "&prop=revisions&rvprop=content&origin=*" ;//Ссылка на модуль MediaWiki
 	var siteTitle = encodeURIComponent(res.entities[id].sitelinks[site].title).replace('%3A',':').replace('%20',' ');//Название модуля на искомом Wiki(можно использовать MediaWiki)
 	var siteUrl = "https://" + site.slice(0,-4) + ".wikipedia.org/w/api.php?format=json&action=query&titles=" + siteTitle + "&prop=revisions&rvprop=content&origin=*" ;//Ссылка на искомый модуль
-	
-	document.write('id: '+id); document.write("<BR>");
 
 	$.get(mediawikiUrl, function(mediawikiJson) {
 		$.get(siteUrl, function(siteJson) {
@@ -24,17 +24,12 @@ $.get('https://www.wikidata.org/w/api.php?action=wbgetentities&ids='+id+'&props=
 			var mediawikiText = mediawikiJson.query.pages[mediawikiPageId].revisions[0]['*'];
 			var siteText = siteJson.query.pages[sitePageId].revisions[0]['*'];
 
-			document.write('<table width="100%" cellspacing="0" cellpadding="5"><tr><td width="50%" valign="top">');
-			document.write('mediawiki<BR>'); 
-			document.write(mediawikiUrl); document.write("<BR>");
-			document.write('<pre>'+mediawikiText+'<\/pre><BR>'); 
-			document.write('</td><td valign="top">');
-			document.write(site+'<BR>'); 
-			document.write(siteUrl); document.write("<BR>");
-			document.write('<pre>'+siteText+'<\/pre><BR>');
-			document.write('</td></tr></table>');
+			document.getElementById('header').innerHTML = '<table width="100%" cellspacing="0" cellpadding="5"><tr><td width="50%" valign="top">mediawiki<br>'+mediawikiUrl+'</td><td valign="top">'+site+'<br>'+siteUrl+'</td></tr></table>';
+			
+			$('#mergely').mergely('lhs', mediawikiText);
+			$('#mergely').mergely('rhs', siteText);
 
-			}).fail(function() { alert("error"); })
 		}).fail(function() { alert("error"); })
 	}).fail(function() { alert("error"); })
+}).fail(function() { alert("error"); })
 
