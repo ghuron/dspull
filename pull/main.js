febf5016ed708770f3262f32f5084df0a68bf473
Dmitry <40275556+ooppsss60@users.noreply.github.com>
2019-07-25 02:56:15 +0300

    Revert "Checking if target latest version exists on mediawiki.org"
    This reverts commit 2ac8ac84721ae01dad767d93125a3b9a7e7a30b8.

--- pull/main.js
+++ pull/main.js
@@ -20,43 +20,22 @@ $.get('https://www.wikidata.org/w/api.php?', {action:'wbgetentities', ids:id, pr
 	var siteTitle = res.entities[id].sitelinks[site].title;//Название модуля на искомом Wiki(можно использовать MediaWiki)
 	var siteUrl = "https://" + site.slice(0,-4) + ".wikipedia.org/w/api.php?action=query&titles=" + encodeURIComponent(siteTitle).replace('%3A',':').replace('%20',' ') + "&prop=revisions&rvprop=content|timestamp" ;//Ссылка на искомый модуль
 	
-	$.get(mediawikiUrl, {rvlimit: 50, format:'json', origin:'*'}, getMediawikiJson);
+	$.get(mediawikiUrl, {format:'json', origin:'*'}, getMediawikiJson);
 	function getMediawikiJson (mediawikiJson) {
-		
-		var mediawikiPageId = Object.keys(mediawikiJson.query.pages)[0];
-		var mediawikiTimestamp = mediawikiJson.query.pages[mediawikiPageId].revisions[0].timestamp;
-		var mediawikiText = mediawikiJson.query.pages[mediawikiPageId].revisions[0]['*'];
-		var mediawikiTextHistory = [];
-		for (var i = 0; i < mediawikiJson.query.pages[mediawikiPageId].revisions.length; i++){
-			mediawikiTextHistory.push(mediawikiJson.query.pages[mediawikiPageId].revisions[i]['*']);
-		};
 
 		$.get(siteUrl, {format:'json', origin:'*'}, getSiteJson);
 		function getSiteJson (siteJson) {
 
-
+			var mediawikiPageId = Object.keys(mediawikiJson.query.pages)[0];
+			var mediawikiTimestamp = mediawikiJson.query.pages[mediawikiPageId].revisions[0].timestamp;
+			var mediawikiText = mediawikiJson.query.pages[mediawikiPageId].revisions[0]['*'];
+			
 			var sitePageId = Object.keys(siteJson.query.pages)[0];
 			var siteTimestamp = siteJson.query.pages[sitePageId].revisions[0].timestamp;
 			var siteText = siteJson.query.pages[sitePageId].revisions[0]['*'];
 
-			var versionLag = -1;
-			for (var i = 0; i < mediawikiTextHistory.length; i++){
-				if (mediawikiTextHistory[i]==siteText){
-					versionLag = i;
-					break;
-				};
-			};
-			console.log(versionLag);
-
 			document.getElementById('lheader').innerHTML = site + '<br>' + siteTitle + '<br>' + siteTimestamp; 
 			document.getElementById('rheader').innerHTML = 'mediawiki<br>' + mediawikiTitle + '<br>' + mediawikiTimestamp;
-			if (versionLag>0){
-			document.getElementById('buttonheader').innerHTML += '<br>Upgrade '+ versionLag +' versions ahead';
-			} else if (versionLag==0){
-			document.getElementById('buttonheader').innerHTML += '<br>Modules are the same';	
-			} else if (versionLag==-1){
-			document.getElementById('buttonheader').innerHTML += '<br>Cant find module in the history';	
-			}
 
 			$('#mergely').mergely('lhs', siteText);
 			$('#mergely').mergely('rhs', mediawikiText);
@@ -68,11 +47,8 @@ $.get('https://www.wikidata.org/w/api.php?', {action:'wbgetentities', ids:id, pr
 				var saveButton = document.getElementById('saveButton');
 				saveButton.addEventListener("click", saveText);	
 				function saveText(){
-
-					if (versionLag == 0){
+					if (siteText == mediawikiText){
 						alert('Modules are the same');
-					} else if (versionLag == -1){
-						alert('Cant find module in the history');
 					} else if (mediawikiDocPageId=="-1"){
 						alert('Doc does not exist');
 					} else {
@@ -82,10 +58,10 @@ $.get('https://www.wikidata.org/w/api.php?', {action:'wbgetentities', ids:id, pr
 						if (mediawikiDocText.indexOf('{{Shared Template Warning|') == -1){
 							alert('Module not shared') ;
 						} else {
-							$.post("https://ru.wikipedia.org/w/api.php",{action :'edit',token : '+\\',text : mediawikiText, title:'Википедия:Песочница', origin:'*'},moduleSaved);
+						$.post("https://ru.wikipedia.org/w/api.php",{action :'edit',token : '+\\',text : mediawikiText, title:'Википедия:Песочница', origin:'*'},moduleSaved);
 						};					
 					};
-
+					
 					function moduleSaved (text){
 						alert('Module saved');
 					};		
