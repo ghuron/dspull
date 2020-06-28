6018c01103aeb5833dc02303bff7176da1ade41b
Andrew Zaikin <andrew.zaikin@gmail.com>
2020-06-28 10:43:09 +0300

    One more update

--- oauth.php
+++ oauth.php
@@ -5,6 +5,6 @@
   use MediaWiki\OAuthClient\Client;
 
   $conf = new ClientConfig( 'https://en.wikipedia.org/w/index.php?title=Special:OAuth' );
-  $conf->setConsumer( new Consumer( 'abef0f582ef20b645630e76cfcfef67b', require_once __DIR__ . '/.secret.php' ) );
+  $conf->setConsumer( new Consumer( 'ef0ed4393a4f093a3a6f92563e80e28f', require_once __DIR__ . '/.secret.php' ) );
   return new Client( $conf );
 ?>
